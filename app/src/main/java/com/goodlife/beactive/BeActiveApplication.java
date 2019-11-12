b7c58bf27979edaade036f422800186e231d339d
bmomani1 <bmomani@atypon.com>
2019-11-12 20:38:26 +0200

    create Application class

---
+++ app/src/main/java/com/goodlife/beactive/BeActiveApplication.java
@@ -0,0 +1,13 @@
+package com.goodlife.beactive;
+
+import android.app.Application;
+
+/**
+ * Created by Basheer Al momani (bmomani@atypon.com) on 11/12/19.
+ */
+public class BeActiveApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
